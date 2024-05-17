647fd54dccd69d3d6a975e19bf4b378e4647f494
Tyson G Fritz <tysongfritz@gmail.com>
2024-05-16 19:06:26 -0700

    Added transactions and GetAccountBalance

--- 01-go-bank/account.go
+++ 01-go-bank/account.go
@@ -1,8 +1,22 @@
 package main
 
+import "time"
+
+type transaction struct {
+	Amount float64
+	OccuredAt time.Time
+}
+
 type Account struct {
 	Name string
 	Number int64
-	Balance float64
-	Transactions []float64
+	Transactions []transaction
+}
+
+func GetAccountBalance(account Account) float64 {
+	balance := 0.0
+	for _, trans := range account.Transactions {
+		balance += trans.Amount
+	}
+	return balance
 }
